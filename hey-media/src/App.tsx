f62161e958eb25f7f46c85162897316335f2f353
Edwin Zhang <edwinzhang64@gmail.com>
2021-04-06 16:34:26 -0600

    Add audio wave

--- hey-media/src/App.tsx
+++ hey-media/src/App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Siriwave from "react-siriwave";
 
 const App: React.FunctionComponent = () => {
   return (
-    <Header>
-      Hey Media
-    </Header>
+    <>
+      <Header>Hey Media</Header>
+      <Siriwave color={"#3D5AFE"} speed={0.15} amplitude={0.7} cover={true} />
+      <Text>Listening for command...</Text>
+    </>
   );
-}
+};
 
 export default App;
 
 const Header = styled.div`
-  font-weight: 600;
-  font-size: 20px;
-  margin: 16px auto;
+  font-weight: 400;
+  font-size: 14px;
+  padding: 8px;
+  text-align: left;
+  background: #f2f3f4;
+`;
+
+const Text = styled.div`
+  font-weight: 400;
+  font-size: 12px;
+  padding-bottom: 16px;
   text-align: center;
 `;
